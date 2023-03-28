61a12e669f2602f0ac9a5649f55e80396939b60c
Hao <justid@users.noreply.github.com>
2023-03-28 22:55:56 +0800

    feat: update dns address to google dns(with edns)

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ addEventListener('fetch', function(event) {
     event.respondWith(response)
 })
 
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://dns.google/dns-query'
+const dohjson = 'https://dns.google/resolve'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
@@ -14,7 +14,7 @@ async function handleRequest(request) {
     const { method, headers, url } = request
     const searchParams = new URL(url).searchParams
     if (method == 'GET' && searchParams.has('dns')) {
-        return await fetch(doh + '?dns=' + searchParams.get('dns'), {
+        return await fetch(doh + '?dns=' + searchParams.get('dns')+'&edns_client_subnet=110.53.162.0/24', {
             method: 'GET',
             headers: {
                 'Accept': contype,
@@ -31,12 +31,25 @@ async function handleRequest(request) {
         });
     } else if (method== 'GET' && headers.get('Accept')==jstontype) {
         const search = new URL(url).search
-         return await fetch(dohjson + search, {
+        const result = await fetch(dohjson + search + '&edns_client_subnet=110.53.162.0/24', {
             method: 'GET',
             headers: {
                 'Accept': jstontype,
             }
         });
+        const secondCheck = await result.clone().json();
+        if (secondCheck['Status'] == 0) {
+            return result
+        } else {
+            // Some request name can't work with edns
+            // "Status": 5 /* REFUSED */,
+            return await fetch(dohjson + search, {
+                method: 'GET',
+                headers: {
+                    'Accept': jstontype,
+                }
+            });
+        }
     } else {
         return new Response("", {status: 404})
     }
